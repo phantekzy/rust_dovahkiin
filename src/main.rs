32b5bc547b280d529bd493ebe30c647f49d8478f
phantekzy <mainilotfi@gmail.com>
2026-02-16 02:14:18 +0100

    setup terminal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::io;
+
+use crossterm::terminal::enable_raw_mode;
+
 mod system;
 mod ui;
 
-fn main() {
-    //
+fn main() -> Result<(), io::Error> {
+    // Setup terminal
+    enable_raw_mode()?;
 }
