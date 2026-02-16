dc3202a86b9b5e13fc6f6520388a60a317e18bd6
phantekzy <mainilotfi@gmail.com>
2026-02-16 02:09:40 +0100

    fixed some bugs

--- src/system.rs
+++ src/system.rs
@@ -1,6 +1,5 @@
 use sysinfo::{CpuExt, System, SystemExt};
 
-#[derive(Debug)]
 pub struct SysStats {
     pub cpu_usage: f32,
     pub mem_used: u64,
@@ -9,9 +8,11 @@ pub struct SysStats {
 
 pub fn get_stats(sys: &mut System) -> SysStats {
     sys.refresh_all();
+
     // Calculate average CPU usage across all cores
     let cpu: f32 =
         sys.cpus().iter().map(|cpu| cpu.cpu_usage()).sum::<f32>() / sys.cpus().len() as f32;
+
     SysStats {
         cpu_usage: cpu,
         mem_used: sys.used_memory(),
