021456be080968e4194853fb05d342ae1c63d543
phantekzy <mainilotfi@gmail.com>
2026-02-16 01:49:46 +0100

    Sysstats

--- src/system.rs
+++ src/system.rs
@@ -12,4 +12,9 @@ pub fn get_stats(sys: &mut System) -> SysStats {
     // Calculate average CPU usage across all cores
     let cpu: f32 =
         sys.cpus().iter().map(|cpu| cpu.cpu_usage()).sum::<f32>() / sys.cpus().len() as f32;
+    SysStats {
+        cpu_usage: cpu,
+        mem_used: sys.used_memory(),
+        mem_total: sys.total_memory(),
+    }
 }
